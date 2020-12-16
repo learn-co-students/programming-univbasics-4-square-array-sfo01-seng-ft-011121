17828bf90f6eca84983ae1a4286fb62638f40f1e
Erik Huang <erikhuangdrums@gmail.com>
2020-12-16 22:56:01 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,11 @@
 def square_array(arr)
   new_arr = []
   i = 0
-  while i < arr.length do
+  
+  while i < arr.length
     new_arr << arr[i] ** 2
     i += 1
   end
-  return new_arr
+  
+  new_arr
 end
